4f981dff9c6464c3e0d3f985c5f53ee2fc39e8cc
Luke Barnard <l.a.barnard@reading.ac.uk>
2021-02-12 13:39:30 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# huxt_ephemeris
+
+
+This script produces the ephemeris file used by HUXt. It produces a HDF5 file of the coordinates of the Earth, Venus, and Mercury, as well as  STEREO-A and STEREO-B, for the period 1963-01-01 until 2029-01-01, at 4 hour resolution. Coordintaes are provided in the Carrington, HEEQ, and HAE systems. Coordinates for STEREO-A and STEREO-B after 2020 are based on a predicted ephemeris, rather than the definitive ephemeris. 
+
---
+++ make_huxt_ephemeris.py
@@ -0,0 +1,56 @@
+# Standard library
+from astropy.time import Time
+import astropy.units as u
+import numpy as np
+import pandas as pd
+import h5py
+# Our own library for using spice with STEREO (https://github.com/LukeBarnard/stereo_spice)
+from stereo_spice.coordinates import StereoSpice
+spice = StereoSpice()
+
+def make_ephemeris():
+    ephem = h5py.File('ephemeris.hdf5', 'w')
+    stereo_begin = Time('2007-01-01T00:00:00')
+
+    bodies = ['EARTH', 'VENUS', 'MERCURY', 'STA', 'STB']
+    for body in bodies:
+
+        body_group = ephem.create_group(body)
+
+        if (body == 'STA') | (body=='STB'):
+            t_start = '2007-01-01T00:00:00'
+        else:
+            t_start = '1963-01-01T00:00:00'
+
+        t_stop = '2029-01-01T00:00:00'
+        times = pd.date_range(t_start, t_stop, freq='4H')
+        times = Time(times.to_pydatetime(), format='datetime')
+
+        for system in ['CARR', 'HEEQ', 'HAE']:
+
+            coord_group = body_group.create_group(system)
+
+            coords = np.zeros((times.size, 3))*np.NaN
+
+            for i, time in enumerate(times):
+
+                if (body in ['STA', 'STB']) & (time < stereo_begin):
+                    continue
+
+                coords[i, :] = spice.get_lonlat(time, body, system, degrees=False)
+
+            coord_group.create_dataset('time', data=times.jd)
+            rad = coord_group.create_dataset('radius', data=coords[:, 0])
+            rad.attrs['unit'] = u.km.to_string()
+            lon = coord_group.create_dataset('longitude', data=coords[:, 1])
+            lon.attrs['unit'] = u.rad.to_string()
+            lat = coord_group.create_dataset('latitude', data=coords[:, 2])
+            lat.attrs['unit'] = u.rad.to_string()
+
+            ephem.flush()
+
+    ephem.close()
+    return
+
+if __name__=="__main__":
+    make_ephemeris()
